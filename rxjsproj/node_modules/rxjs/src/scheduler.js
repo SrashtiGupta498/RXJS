712217c52c8fc358820ed046f5bdd2372e59e14e
srashtiGupta <srashtig1009@gmail.com>
2023-02-28 20:02:56 +0530

    hyy001

---
+++ rxjsproj/node_modules/rxjs/src/scheduler.js
@@ -0,0 +1,18 @@
+import { Observable, asyncScheduler } from 'rxjs';
+import { observeOn } from 'rxjs/operators';
+
+var observable = new Observable(function subscribe(subscriber) {
+   subscriber.next("My First Observable");
+   subscriber.next("Testing Observable");
+   subscriber.complete();
+}).pipe(
+   observeOn(asyncScheduler)
+);
+console.log("Observable Created");
+observable.subscribe(
+   x => console.log(x),
+   (e)=>console.log(e),
+   ()=>console.log("Observable is complete")
+);
+
+console.log('Observable Subscribed');
